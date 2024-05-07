7ce07c7fae453507f147b5001f43d72c96aa9fed
joytantrum <lindsayc098@gmail.com>
2024-05-07 16:02:55 -0400

    Initial Commit

---
+++ Trusted-Tarot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trusted-Tarot
+//
+//  Created by Lindsay Clifford on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trusted-Tarot/Trusted_TarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trusted_TarotApp.swift
+//  Trusted-Tarot
+//
+//  Created by Lindsay Clifford on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Trusted_TarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
